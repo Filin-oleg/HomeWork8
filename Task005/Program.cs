3bc47206d0cabf28aa01afa7844c63706904652a
Filin-oleg <110037084+Filin-oleg@users.noreply.github.com>
2022-08-31 19:12:18 +0200

    done

---
+++ Task005/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4. 
+
+Console.Clear();
+Console.WriteLine("Введите размер квадратного двумерного массива ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, n];
+
+void FillMatrix (int[,] matrix)
+{
+int temp = 1;
+int i = 0;
+int j = 0;
+while (temp <= matrix.GetLength(0) * matrix.GetLength(1))
+{
+  matrix[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= matrix.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > matrix.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+}
+void PrintArray (int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      if (matrix[i,j] / 10 <= 0)
+      Console.Write($" {matrix[i,j]} ");
+      else Console.Write($"{matrix[i,j]} ");
+    }
+    Console.WriteLine();
+  }
+}
+FillMatrix(matrix);
+PrintArray(matrix);
+
